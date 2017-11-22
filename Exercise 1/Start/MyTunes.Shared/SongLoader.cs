795d2df7225253b2821219d59008bb7214bc47eb
Alison Jonck <alison.jonck@totvspartners.com.br>
2017-11-22 21:48:53 -0200

    added OpenData definition at SongLoader

--- Exercise 1/Start/MyTunes.Shared/SongLoader.cs
+++ Exercise 1/Start/MyTunes.Shared/SongLoader.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
+using System;
 
 namespace MyTunes
 {
@@ -11,16 +12,33 @@ namespace MyTunes
 
 		public static async Task<IEnumerable<Song>> Load()
 		{
-			using (var reader = new StreamReader(OpenData()))
+			using (var reader = new StreamReader(await OpenData()))
 			{
 				return JsonConvert.DeserializeObject<List<Song>>(await reader.ReadToEndAsync());
 			}
 		}
 
-		private static Stream OpenData()
+		private async static Task<Stream> OpenData()
 		{
-			// TODO: add code to open file here.
-			return null;
+		#if __IOS__
+
+			return await Task.FromResult(File.OpenRead(Filename));
+
+		#elif __ANDROID__
+
+			return await Task.FromResult(Android.App.Application.Context.Assets.Open(Filename));
+
+		#elif WINDOWS_PHONE_APP
+
+			var sf = await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFileAsync(Filename);
+			return await sf.OpenStreamForReadAsync();
+
+		#elif WINDOWS_UWP
+
+			var sf = await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFileAsync(Filename);
+			return await sf.OpenStreamForReadAsync();
+
+		#endif
 		}
 	}
 }
